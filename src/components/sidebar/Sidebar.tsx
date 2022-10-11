5e78114ea444ecba43186115f5d3a6b4b2c591ee
Nkwo Chukwuemeka <emekankwo49@gmail.com>
2022-10-11 01:45:51 +0100

    added dashboard and user routing to the sidebar

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -7,6 +7,8 @@ import {
 } from "./SidebarItems";
 import { BsArrowBarRight, BsArrowBarLeft } from "react-icons/bs";
 import "./sidebar.css";
+import { briefcase, side_drop } from "../../assets";
+import { NavLink } from "react-router-dom";
 
 export default function Sidebar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -24,7 +26,18 @@ export default function Sidebar() {
       <div className="sidebar" style={{ width: isOpen ? "300px" : "65px" }}>
         <div className="top_section">
           <div
-            style={{ marginLeft: isOpen ? "90%" : "0px" }}
+            className="top_section_header"
+            style={{ display: isOpen ? "flex" : "none" }}
+          >
+            <div className="header_logo">
+              <img src={briefcase} alt="" />
+            </div>
+            <span>
+              Switch Organization <img src={side_drop} alt="" />
+            </span>
+          </div>
+          <div
+            style={{ marginLeft: isOpen ? "" : "0px" }}
             className="hamburger"
             onClick={toggle}
           >
@@ -37,18 +50,15 @@ export default function Sidebar() {
             {TopItem.map((item: SideItemProps, index: number) => {
               return (
                 <div className="link-item" key={index}>
-                  <span
-                    className="name"
-                    // style={{ display: isOpen ? "block" : "none" }}
-                  >
-                    {item.icon}
-                  </span>
-                  <span
-                    className="name"
-                    style={{ display: isOpen ? "block" : "none" }}
-                  >
-                    {item.name}
-                  </span>
+                  <NavLink to={item.link} className="link-items">
+                    <img src={item.icon} alt={item.name} />
+                    <span
+                      className="name"
+                      style={{ display: isOpen ? "block" : "none" }}
+                    >
+                      {item.name}
+                    </span>
+                  </NavLink>
                 </div>
               );
             })}
@@ -61,18 +71,15 @@ export default function Sidebar() {
                   <h5 style={{ display: isOpen ? "block" : "none" }}>
                     {item.header}
                   </h5>
-                  <span
-                    className="icon"
-                    // style={{ display: isOpen ? "block" : "none" }}
-                  >
-                    {item.icon}
-                  </span>
-                  <span
-                    className="name"
-                    style={{ display: isOpen ? "block" : "none" }}
-                  >
-                    {item.name}
-                  </span>
+                  <NavLink to={item.link} className="link-items ">
+                    <img src={item.icon} alt={item.name} />
+                    <span
+                      className="name"
+                      style={{ display: isOpen ? "block" : "none" }}
+                    >
+                      {item.name}
+                    </span>
+                  </NavLink>
                 </div>
               );
             })}
@@ -84,41 +91,35 @@ export default function Sidebar() {
                   <h5 style={{ display: isOpen ? "block" : "none" }}>
                     {item.header}
                   </h5>
-                  <span
-                    className="icon"
-                    // style={{ display: isOpen ? "block" : "none" }}
-                  >
-                    {item.icon}
-                  </span>
-                  <span
-                    className="name"
-                    style={{ display: isOpen ? "block" : "none" }}
-                  >
-                    {item.name}
-                  </span>
+                  <NavLink to={item.link} className="link-items ">
+                    <img src={item.icon} alt={item.name} />
+                    <span
+                      className="name"
+                      style={{ display: isOpen ? "block" : "none" }}
+                    >
+                      {item.name}
+                    </span>
+                  </NavLink>
                 </div>
               );
             })}
           </div>
-          <div className="links">
+          <div className="links active">
             {SettingItems.map((item: SideItemProps, index: number) => {
               return (
                 <div className="link-item" key={index}>
                   <h5 style={{ display: isOpen ? "block" : "none" }}>
                     {item.header}
                   </h5>
-                  <span
-                    className="icon"
-                    // style={{ display: isOpen ? "block" : "none" }}
-                  >
-                    {item.icon}
-                  </span>
-                  <span
-                    className="name"
-                    style={{ display: isOpen ? "block" : "none" }}
-                  >
-                    {item.name}
-                  </span>
+                  <NavLink to={item.link} className="link-items ">
+                    <img src={item.icon} alt={item.name} />
+                    <span
+                      className="name"
+                      style={{ display: isOpen ? "block" : "none" }}
+                    >
+                      {item.name}
+                    </span>
+                  </NavLink>
                 </div>
               );
             })}
