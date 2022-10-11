ede55e27ecbeab32a910fd87cafcd4b63344fcf8
Nkwo Chukwuemeka <emekankwo49@gmail.com>
2022-10-11 01:44:14 +0100

    added the dashboard and users route

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./App.css";
-import { Dashboard, Login, User, UserDetails } from "./pages";
+import { Main, Login, User, UserDetails, Dashboard } from "./pages";
 
 function App() {
   return (
@@ -10,8 +10,9 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Login />} />
-          <Route path="dashboard" element={<Dashboard />}>
-            <Route index element={<User />} />
+          <Route path="dashboard" element={<Main />}>
+            <Route index element={<Dashboard />} />
+            <Route path="users" element={<User />} />
             <Route path="users/:id" element={<UserDetails />} />
           </Route>
         </Routes>
